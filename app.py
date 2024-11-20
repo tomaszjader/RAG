80bce8bc060d88d2ca92edb00b674440a3734e18
Tomasz Jąder <tomasz.jader@gmail.com>
2024-11-20 23:23:35 +0100

    refactoring code

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import hashlib
 from dotenv import load_dotenv
 
 load_dotenv()
-def create(name):
-    response = requests.put(url=url, json=vectors)
-    return response
 
-def add(name):
-    response = requests.put(url=url, json=vectors)
-    return response
+def create(url, json):
+    response = requests.put(url=url, json=json)
+    return response.text
+
+def add(url, json):
+    response = requests.put(url=f"{url}/points", json=json)
+    return response.text
 
 def embed(input):
     response = client.embeddings.create(
@@ -20,23 +21,30 @@ def embed(input):
     )
     return response.data[0].embedding
 
+def indexing(linie, url):
+    for i in linie:
+        json = {"points": [{'id': hashlib.md5(i.encode()).hexdigest(), "payload":{'text': i}, "vector": embed(i)}]}
+        r = add(url=url, json=json)
+    return
+
+def search(url, query):
+    json = {"vector": embed(query),"limit": 1,"with_payload": True}
+    response = requests.post(url=f'{url}/points/search', json=json)
+    return response.text
+
+
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 url = 'http://localhost:6333/collections/wiedza'
 vectors = {"vectors": {"size": 1536,"distance": "Cosine"}}
+create(url, vectors)
+
 with open('baza.txt', 'r', encoding='utf-8') as plik:
-    # Wczytaj linie do listy
     linie = plik.readlines()
 
 
 linie = [linia.strip() for linia in linie]
+indexing(linie, url)
 
-# for i in linie:
-#     json = {"points": [{'id': hashlib.md5(i.encode()).hexdigest(), "payload":{'text': i}, "vector": embed(i)}]}
-     
-#     print(json)
-#     r = requests.put(url=f'http://localhost:6333/collections/wiedza/points', json=json)
-#     print(r)
-pytanie = embed("jak ma na imie pies Adama?")
-q_json = {"vector": pytanie,"limit": 1,"with_payload": True}
-q = requests.post(url=f'{url}/points/search', json=q_json)
-print(q.text)
+pytanie = "jak ma na imie pies Adamsia?"
+
+print(search(url, pytanie))
